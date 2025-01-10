aed6631efefb8545d0b1bfc573d75cbcc96b818a
Mohammed Alakhras <MabdMAk@gmail.com>
2025-01-11 00:28:28 +0300

    Use express-async-handler with Author & finish Author APIs

--- models/Author.js
+++ models/Author.js
@@ -1,46 +1,69 @@
 const mongoose = require("mongoose");
+const { object } = require("joi");
+const Joi = require("joi");
 
-const AuthorSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlenghth: 200,
+const AuthorSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlenghth: 200,
+    },
+    lastnamme: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlenghth: 200,
+    },
+    nationality: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlenghth: 20,
+    },
+    image: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlenghth: 200,
+    },
   },
-  lastnamme: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlenghth: 200,
-  },
-  nationality: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 2,
-    maxlenghth: 20,
-  },
-  image: {
-    type: String,
-    trim: true,
-    minlength: 3,
-    maxlenghth: 200,
-  }}
-  ,{
-
-    timestamps:true
+  {
+    timestamps: true,
   }
 );
-
 
 /*we will pass in first param "ModelName"
  that will create collection in same name in DB.
  Second param is Schema object
 */
-const Author= mongoose.model('Author',AuthorSchema);
+const Author = mongoose.model("Author", AuthorSchema);
 
-module.exports={
-  Author
+function CheckInsertAuthor(data) {
+  const schema = Joi.object({
+    firstname: Joi.string().trim().max(30).min(2).required(),
+    lastnamme: Joi.string().trim().max(30).min(2).required(),
+    nationality: Joi.string().trim().max(30).min(2).required(),
+    image: Joi.string().trim(),
+  });
+  return schema.validate(data);
 }
+
+function CheckUpdatetAuthor(data) {
+  const schema = Joi.object({
+    firstname: Joi.string().trim().max(30).min(2),
+    lastnamme: Joi.string().trim().max(30).min(2),
+    nationality: Joi.string().trim().max(30).min(2),
+    image: Joi.string().trim(),
+  });
+  return schema.validate(data);
+}
+module.exports = {
+  Author,
+  CheckInsertAuthor,
+  CheckUpdatetAuthor,
+};
--- routes/authors.js
+++ routes/authors.js
@@ -1,32 +1,14 @@
 const express = require("express");
-const { object } = require("joi");
 
 const router = express.Router();
-const Joi = require("joi");
-const { Author } = require("../models/Author");
-const authors = [
-  {
-    id: 1,
-    firstname: "Mohammed",
-    lastnamme: "Alakhras",
-    nationality: "Syria",
-    image: "1.png",
-  },
-  {
-    id: 2,
-    firstname: "Ahmed",
-    lastnamme: "Hazem",
-    nationality: "Egypt",
-    image: "2.png",
-  },
-  {
-    id: 3,
-    firstname: "Alia",
-    lastnamme: "Maher",
-    nationality: "Lebanon",
-    image: "3.png",
-  },
-];
+const mongoose=require('mongoose')
+const {
+  Author,
+  CheckInsertAuthor,
+  CheckUpdatetAuthor,
+} = require("../models/Author");
+
+const asyncHandler = require("express-async-handler");
 
 /**
  *@description get all authors
@@ -35,9 +17,13 @@ const authors = [
  *@access public
  */
 
-router.get("/", (req, res) => {
-  res.status(200).json(authors);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const authorList = await Author.find().sort({ firstname: 1 }); //.select('firstname lastname');
+    res.status(200).json(authorList);
+  })
+);
 
 /**
  *@description get author by id
@@ -46,14 +32,25 @@ router.get("/", (req, res) => {
  *@access public
  */
 
-router.get("/:id", (req, res) => {
-  const auth = authors.find((e) => e.id === parseInt(req.params.id));
-  if (auth) {
-    res.status(200).json(auth);
-  } else {
-    res.status(404).json({ message: "Not found" });
-  }
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+ 
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+
+    const auth = await Author.findById(id);
+
+    if (auth) {
+      res.status(200).json(auth);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  })
+);
 
 /**
  *@description get specific author by id
@@ -62,23 +59,25 @@ router.get("/:id", (req, res) => {
  *@access public
  */
 
-router.post("/", async (req, res) => {
-  const { err } = CheckInsertAuthor(req.body);
-  if (err) {
-    res.status(400).json({ message: err.details[0].message });
-  } else {
-  }
-  const auth = new Author({
-    id: authors.length + 1,
-    firstname: req.body.firstname,
-    lastnamme: req.body.lastnamme,
-    nationality: req.body.nationality,
-    image: req.body.image,
-  });
-  const result =await auth.save();
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { err } = CheckInsertAuthor(req.body);
+    if (err) {
+      res.status(400).json({ message: err.details[0].message });
+    } else {
+    }
+    const auth = new Author({
+      firstname: req.body.firstname,
+      lastnamme: req.body.lastnamme,
+      nationality: req.body.nationality,
+      image: req.body.image,
+    });
+    const result = await auth.save();
 
-  res.status(201).json(result);
-});
+    res.status(201).json(result);
+  })
+);
 
 /**
  *@description Update Author
@@ -87,21 +86,44 @@ router.post("/", async (req, res) => {
  *@access public
  */
 
-router.put("/:id", (req, res) => {
-  const { err } = CheckUpdatetAuthor(req.body);
+router.put(
+  "/:id",
 
-  if (err) {
-    return res.status(400).json({ message: err.details[0].message });
-  }
+  asyncHandler(async (req, res) => {
+    const { err } = CheckUpdatetAuthor(req.body);
 
-  const author = authors.find((b) => b.id === parseInt(req.params.id));
+    if (err) {
+      return res.status(400).json({ message: err.details[0].message });
+    }
 
-  if (author) {
-    res.status(200).json({ message: "Updated " });
-  } else {
-    res.status(404).json({ message: "Not Found " });
-  }
-});
+    // const author = authors.find((b) => b.id === parseInt(req.params.id));
+
+    // if (author) {
+    //   res.status(200).json({ message: "Updated " });
+    // } else {
+    //   res.status(404).json({ message: "Not Found " });
+    // }
+
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+      res.status(400).json({ message: "Invalid ID format" });
+    }
+    const author = await Author.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          nationality: req.body.nationality,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json(author);
+  })
+);
 
 /**
  *@description delete author
@@ -109,33 +131,22 @@ router.put("/:id", (req, res) => {
  *@method delete
  *@access public
  */
-router.delete("/:id", (req, res) => {
-  const author = authors.find((b) => b.id === parseInt(req.params.id));
-  if (author) {
-    res.status(200).json({ msg: "Deleted" });
-  } else {
-    res.status(400).json({ mgs: "not found" });
-  }
-});
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    if(!mongoose.Types.ObjectId.isValid(id)){
+      res.status(400).json({ message: "Invalid ID format" });
+    }
+    
+    const author = await Author.findById(req.params.id);
 
-function CheckInsertAuthor(data) {
-  const schema = Joi.object({
-    firstname: Joi.string().trim().max(30).min(2).required(),
-    lastnamme: Joi.string().trim().max(30).min(2).required(),
-    nationality: Joi.string().trim().max(30).min(2).required(),
-    image: Joi.string().trim(),
-  });
-  return schema.validate(data);
-}
-
-function CheckUpdatetAuthor(data) {
-  const schema = Joi.object({
-    firstname: Joi.string().trim().max(30).min(2),
-    lastnamme: Joi.string().trim().max(30).min(2),
-    nationality: Joi.string().trim().max(30).min(2),
-    image: Joi.string().trim(),
-  });
-  return schema.validate(data);
-}
+    if (author) {
+      await Author.findByIdAndDelete(req.params.id);
+      res.status(200).json({ msg: "Deleted" });
+    } else {
+      res.status(400).json({ mgs: "not found" });
+    }
+  })
+);
 
 module.exports = router;
